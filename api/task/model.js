ce2ba74d9ffe0fbbd058f76b8a553eaa271b48a4
Mitchell Walker <eagleye1144@gmail.com>
2021-11-05 16:01:31 -0600

    still may need seed file

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,32 @@
 const db = require('../../data/dbConfig')
+
+async function getTasks() {
+    let dataForTasks = await db("tasks as t")
+      .select("t.*", "p.project_name", "p.project_description")
+      .join("projects as p", "t.project_id", "p.project_id");
+  
+    dataForTasks.forEach((rec) => {
+      if (rec.task_completed === 0) {
+        rec.task_completed = false;
+      } else {
+        rec.task_completed = true;
+      }
+    });
+  
+    return dataForTasks;
+    
+  }
+  
+  async function createTask(task) {
+    const [id] = await db("tasks").insert(task);
+    const newTask = await db("tasks").where("task_id", id);
+  
+    newTask[0].task_completed = newTask[0].task_completed === 0 ? false : true;
+  
+    return newTask[0];
+  }
+  
+  module.exports = {
+    getTasks,
+    createTask,
+  };
--- api/task/router.js
+++ api/task/router.js
@@ -1,4 +1,23 @@
 const router = require("express").Router()
+const Tasks = require('./model');
+
+router.get('/', async (req, res, next) => {
+    try {
+        const taskList = await Tasks.getTasks()
+        res.status(200).json(taskList)
+    } catch(err){
+        next(err)
+    }
+})
+
+router.post('/', async (req, res, next) => {
+    try {
+        const newTask = await Tasks.createTask(req.body)
+        res.status(201).json(newTask)
+    } catch (err) {
+        next(err)
+    }      
+})
 
 
 router.use((err, req, res, next)=>{
