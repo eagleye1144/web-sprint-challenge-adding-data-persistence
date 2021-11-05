a4a619ef24dae4c075ea5e99721ee90613a78b8d
Mitchell Walker <eagleye1144@gmail.com>
2021-11-05 13:27:43 -0600

    Server up

--- api/server.js
+++ api/server.js
@@ -1,1 +1,10 @@
-// build your server here and require it from index.js
+const express = require('express')
+
+const server = express()
+
+server.use(express.json())
+
+server.use('*', (req, res) => {
+    res.json({api: 'up'})
+})
+module.exports = server
