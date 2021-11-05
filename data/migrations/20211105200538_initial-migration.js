7c8ac5b0bc6dc536ec281200e9621098ff4276ec
Mitchell Walker <eagleye1144@gmail.com>
2021-11-05 16:05:19 -0600

    Added resource description

--- data/migrations/20211105200538_initial-migration.js
+++ data/migrations/20211105200538_initial-migration.js
@@ -10,6 +10,7 @@ exports.up = async function(knex) {
   .createTable('resources', table => {
       table.increments('resource_id')
         table.string('resource_name').notNullable().unique()
+        table.string('resource_description')
   })
   .createTable('tasks', table => {
       table.increments('task_id')
